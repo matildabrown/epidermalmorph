0844407f5a41c7b9ee5bb9ec6f01e34a18d61e85
matildabrown <matilda.brown@utas.edu.au>
2021-08-02 12:25:56 +1000

    Fix version control (last one old files)

--- R/image_to_poly.r
+++ R/image_to_poly.r
@@ -1,45 +1,62 @@
 #' Convert image to SpatialPolygons
 #'
 #' @param dir String. Directory for images.
-#' @param fname String. File name.
+#' @param file String. File name.
 #'
 #'#' @return A list with components:
 #' \describe{
-#' \item{pave}{SpatialPolygonsDataFrame. Pavement cells. }
-#' \item{stom}{SpatialPolygonsDataFrame. Stomata (pore + guard cells).}
-#' \item{subs}{SpatialPolygonsDataFrame. Lateral subsidiary cells. }
+#' \item{cells}{SpatialPolygonsDataFrame. All cells, with values stored in a 'value' column.}
+#' \item{junction_coords}{Dataframe with two columns: x and y coordinates of tri-cell junctions.}
 #' }
-#' @return List of SpatialPolygonsDataFrames: pavement cells, subsidiary cells and stomata.
-#' @details This function assumes that the pixels are coloured in the following order
-#' (darkest to lightest): wall, stomate, subsidiary, pavement.
 #'
 #' @import methods
 #'
 #' @export
 
 
-image_to_poly <- function(dir, fname){
+image_to_poly <- function(dir, file){
 
-  im_st <- stars::read_stars(paste0(dir, "/", fname))[,]
+  im_st <- stars::read_stars(paste0(dir, "/", file))[,]
   p <-  sf::st_as_sf(im_st, as_points = FALSE, merge = TRUE)
   values <- sf::st_drop_geometry(p)
-  table(values)
-  t <-  table(values)
+  p <- as(p, "Spatial")
 
-  # t <- t[order(t)]
+  colnames(p@data) <- "value"
 
-  paveno <- which(values==as.numeric(names(t)[4]))
-  stomno <- which(values==as.numeric(names(t)[2]))
-  subsno <- which(values==as.numeric(names(t)[3]))
-  p1 <- p[paveno,]
-  p2 <- p[stomno,]
-  p3 <- p[subsno,]
-  pave <- as(p1, "Spatial")
-  stom <- as(p2, "Spatial")
-  subs <- as(p3, "Spatial")
-  pave <- pave[which(rgeos::gArea(pave, byid = T)>5),] #remove rogue pixels
-  stom <- stom[which(rgeos::gArea(stom, byid = T)>5),] #remove rogue pixels
-  subs <- subs[which(rgeos::gArea(subs, byid = T)>5),] #remove rogue pixels
+  p.cells <- p[which(p$value!=0),]
+  p.cells <- p.cells[which(rgeos::gArea(p.cells, byid = T)>10),]
+  walls <- p[which(values==0),]
 
-  return(list(pave=pave, stom=stom, subs=subs))
+  im_st_walls <- im_st[[1]]
+  im_st_walls[which(im_st_walls>0)] <- 100
+  im_st_walls[which(im_st_walls==0)] <- 255
+  im_st_walls[which(im_st_walls==100)] <- 0
+
+  man <- imager::cimg2magick(imager::as.cimg(im_st_walls))
+  jcn <- man %>%
+   image_flop() %>%
+   image_morphology('Thinning','Skeleton') %>%
+   image_morphology('Thinning','Corners') %>%
+   image_morphology('Thinning','LineEnds', iterations=-1) %>%
+   image_morphology('Thinning','Skeleton') %>%
+   image_morphology('Thinning','Diagonals') %>%
+   image_morphology('Thinning','Corners') %>%
+   image_morphology("HMT","LineJunctions")
+
+
+
+  jcn_data <- t(matrix(as.integer(jcn[[1]]), nrow=attributes(im_st)$dimensions$y$to))
+  #jcn_data[which(skel==0)] <- 0
+
+
+
+#plot(imager::as.cimg(jcn_points), interpolate = F)
+jcncoords <- setNames(reshape2::melt(jcn_data),c("x","y","val"))
+jcncoords <- jcncoords[which(jcncoords$val!=0 & jcncoords$x<=attributes(im_st)$dimensions$x$to),1:2]
+jcncoords$y <- attributes(im_st)$dimensions$y$to - jcncoords$y
+
+  return(list(cells=p.cells, junction_coords=jcncoords) )
 }
+
+
+
